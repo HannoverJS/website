efe54c56d034c6d37eb4f2a3b6eef1bce1760d22
Tim Cheung <tim@cheung.io>
2016-08-28 14:16:51 +0200

    use dynamic twitter profile images generation

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -4,37 +4,36 @@ import Avatar from '../Avatar'
 import TwitterLink from '../TwitterLink'
 import styles from './styles.css'
 
+function getTwitterProfileImage(screenName) {
+  return `https://twitter.com/${screenName}/profile_image?size=original`
+}
+
 const team = [
   {
-    avatar: 'https://pbs.twimg.com/profile_images/657317643515490304/DDwp-N7D_400x400.jpg',
     name: 'Christoph Burgdorf',
     job: 'Trainer',
     company: 'thoughtram',
     twitter: 'cburgdorf'
   },
   {
-    avatar: 'https://pbs.twimg.com/profile_images/740876520437211136/tpm5d6Ya_400x400.jpg',
     name: 'Pascal Precht',
     job: 'Trainer',
     company: 'thoughtram',
     twitter: 'PascalPrecht'
   },
   {
-    avatar: 'https://pbs.twimg.com/profile_images/605066996850507776/PiUgd6zK_400x400.jpg',
     name: 'Jan-Oliver Pantel',
     job: 'Sr. Software Engineer',
     company: 'NewStore Inc.',
     twitter: 'JanPantel'
   },
   {
-    avatar: 'https://pbs.twimg.com/profile_images/640230245468098560/AWsMiFL9_400x400.jpg',
     name: 'Robin Thrift',
     job: 'Software Engineer',
     company: 'NewStore Inc.',
     twitter: 'RobinThrift'
   },
   {
-    avatar: 'https://pbs.twimg.com/profile_images/701526291103748098/0bYO7Xq0_400x400.png',
     name: 'Tim Cheung',
     job: 'Software Engineer',
     company: 'NewStore Inc.',
@@ -44,11 +43,11 @@ const team = [
 
 export default function Team() {
   const renderMembers = () => team.map((member, i) => {
-    const { avatar, name, job, company, twitter } = member
+    const { name, job, company, twitter } = member
 
     return (
       <li className={styles.member} key={i}>
-        <Avatar src={avatar} alt={name} />
+        <Avatar src={getTwitterProfileImage(twitter)} alt={name} />
         <div className={styles.name}>
           {name}
         </div>
