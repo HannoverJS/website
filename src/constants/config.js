1c3da0b68428191a1fffdb6bb64e6f63749785f8
Tim Cheung <tim@cheung.io>
2016-11-28 19:12:27 +0100

    use hannoverjs-api production url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
-const API_BASE_URL = 'https://hannoverjs-wsmlmvlbnj.now.sh'
+const API_BASE_URL = 'https://hannoverjs-api.now.sh'
 
 export const API = {
-  upcomingMeetups: `${API_BASE_URL}/upcoming/meetups`,
-  upcomingTalks: `${API_BASE_URL}/upcoming/talks`
+  upcomingMeetups: `${API_BASE_URL}/meetups/upcoming`,
+  upcomingTalks: `${API_BASE_URL}/talks/upcoming`
 }
