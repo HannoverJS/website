d05f90c6400367e73983e998253c692e64ffc3ef
Tim Cheung <tim@cheung.io>
2016-11-30 18:23:10 +0100

    update api endpoints

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = 'https://hannoverjs-api.now.sh'
 
 export const API = {
-  upcomingMeetups: `${API_BASE_URL}/meetups/upcoming`,
-  upcomingTalks: `${API_BASE_URL}/talks/upcoming`
+  upcomingMeetups: `${API_BASE_URL}/meetups`,
+  upcomingTalks: `${API_BASE_URL}/talks`
 }
