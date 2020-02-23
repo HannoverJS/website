09b6eef2c141a2899a85e83170b572be01fa39c3
Robin Thrift <RobinThrift@users.noreply.github.com>
2020-02-23 11:23:23 +0000

    Add GitHub actions file

--- deploy.sh
+++ deploy.sh
@@ -11,45 +11,43 @@ fi
  
 REPO=$(git config remote.origin.url)
  
-if [ -n "$TRAVIS_BUILD_ID" ]; then
-  # When running on Travis we need to use SSH to deploy to GitHub
-  #
-  # The following converts the repo URL to an SSH location,
-  # decrypts the SSH key and sets up the Git config with
-  # the correct user name and email (globally as this is a
-  # temporary travis environment)
-  #
-  # Set the following environment variables in the travis configuration (.travis.yml)
-  #
-  #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
-  #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
-  #   GIT_NAME         - The Git user name
-  #   GIT_EMAIL        - The Git user email
-  #
-  echo DEPLOY_BRANCH: $DEPLOY_BRANCH
-  echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
-  echo GIT_NAME: $GIT_NAME
-  echo GIT_EMAIL: $GIT_EMAIL
-  if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
-    echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
-    exit 0
-  else
-    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-      echo "Travis should not deploy from pull requests"
-      exit 0
-    else
-      # switch both git and https protocols as we don't know which travis
-      # is using today (it changed!)
-      REPO=${REPO/git:\/\/github.com\//git@github.com:}
-      REPO=${REPO/https:\/\/github.com\//git@github.com:}
-      
-      chmod 600 $SSH_KEY
-      eval `ssh-agent -s`
-      ssh-add $SSH_KEY
-      git config --global user.name "$GIT_NAME"
-      git config --global user.email "$GIT_EMAIL"
-    fi
-  fi
+# When running on Travis we need to use SSH to deploy to GitHub
+#
+# The following converts the repo URL to an SSH location,
+# decrypts the SSH key and sets up the Git config with
+# the correct user name and email (globally as this is a
+# temporary travis environment)
+#
+# Set the following environment variables in the travis configuration (.travis.yml)
+#
+#   DEPLOY_BRANCH    - The only branch that Travis should deploy from
+#   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
+#   GIT_NAME         - The Git user name
+#   GIT_EMAIL        - The Git user email
+#
+echo DEPLOY_BRANCH: $DEPLOY_BRANCH
+echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
+echo GIT_NAME: $GIT_NAME
+echo GIT_EMAIL: $GIT_EMAIL
+if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
+exit 0
+else
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Travis should not deploy from pull requests"
+  exit 0
+else
+  # switch both git and https protocols as we don't know which travis
+  # is using today (it changed!)
+  REPO=${REPO/git:\/\/github.com\//git@github.com:}
+  REPO=${REPO/https:\/\/github.com\//git@github.com:}
+  
+  chmod 600 $SSH_KEY
+  eval `ssh-agent -s`
+  ssh-add $SSH_KEY
+  git config --global user.name "$GIT_NAME"
+  git config --global user.email "$GIT_EMAIL"
+fi
 fi
  
 REPO_NAME=$(basename $REPO)
